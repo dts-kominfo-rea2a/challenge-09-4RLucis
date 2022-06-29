cb3133d4455f67282ff150ccba43a3d5a8d798aa
Samuel Budy Dharwan <dharwan.helena@gmail.com>
2022-06-29 20:44:59 +0700

    Challenge-09 completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  return (
+    <>
+      <div className="card-contact">
+        <img src={props.data.photo} alt={props.data.name} />
+        <div className="card-contact-detail">
+          <h3>{props.data.name}</h3>
+          <p>{props.data.phone}</p>
+          <p>{props.data.email}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <div className="contact-header">
+        <h2>Call a Friend</h2>
+        <p>your friendly contact app</p>
+        <hr />
+      </div>
+    </>
+  );
+};
 
 export default Header;
